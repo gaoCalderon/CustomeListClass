72c975633037ce6793c762a6381bc9b1ae11a368
gaoCalderon <gao.vang86@gmail.com>
2016-08-22 21:02:59 -0500

    add some

---
+++ CustomListClass/CustomListClass/iterable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    class iterable<T>
+    {
+
+        T shoes;
+
+        public iterable(T itemToPrint)
+        {
+            shoes = itemToPrint;
+        }
+
+        public void ArrayList()
+        {
+            List<string> types = new List<string>();
+            types.Add("classy");
+            types.Add("skimpy");
+            types.Add("casual");
+            types.Add("winter");
+            types.Add("comfy");
+            types.Add("summer");
+
+            Console.WriteLine("I love my {0} & {1} shoes.", types[0], types[2]);
+
+        }
+    }
+}
